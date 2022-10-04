a0de726acc85794d027ba0f231ac8da22ab5af04
usmaJulian <jeusmag86@gmail.com>
2022-10-04 13:11:51 -0500

    04/10/2022 therms, policy

--- lib/src/screens/legal/privacy_policy_screen.dart
+++ lib/src/screens/legal/privacy_policy_screen.dart
@@ -1,65 +1,84 @@
+import 'dart:io';
+
 import 'package:ebloqs_app/src/global/policy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class PrivacyPolicyScreen extends StatelessWidget {
+class PrivacyPolicyScreen extends StatefulWidget {
   static const routeName = 'PrivacyPolicyScreen';
   const PrivacyPolicyScreen({super.key});
+
   @override
+  State<PrivacyPolicyScreen> createState() => _PrivacyPolicyScreenState();
+}
+
+class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: const Text(
-          'Políticas de privacidad',
-          style: TextStyle(
-            color: Color(0xff170658),
-            fontSize: 17,
-            fontFamily: 'Archivo',
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-        leadingWidth: size.width * 0.128,
-        leading: Padding(
-          padding: EdgeInsets.only(left: size.width * 0.064),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: SvgPicture.asset(
-              'assets/Vectores/Iconos/Arrow left.svg',
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: const Text(
+            'Políticas de privacidad',
+            style: TextStyle(
+              color: Color(0xff170658),
+              fontSize: 17,
+              fontFamily: 'Archivo',
+              fontWeight: FontWeight.w700,
             ),
           ),
-        ),
-      ),
-      body: SingleChildScrollView(
-          child: Column(
-        children: [
-          Container(
-            width: size.width,
-            height: 0.50,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: const Color(0xffc9d1d1),
-                width: 0.50,
+          leadingWidth: size.width * 0.128,
+          leading: Padding(
+            padding: EdgeInsets.only(left: size.width * 0.064),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: SvgPicture.asset(
+                'assets/Vectores/Iconos/Arrow left.svg',
               ),
             ),
           ),
-          Padding(
-              padding: EdgeInsets.only(
-                  left: size.width * 0.04,
-                  right: size.width * 0.04,
-                  top: size.height * 0.0357142857142857),
-              child: Center(
-                child: Text(politicas),
-              )),
-        ],
-      )),
-    );
+        ),
+        body: const WebView(
+          initialUrl: 'https://ebloqs.com/ebloqs-token/politicas-de-privacidad',
+        )
+
+        //  SingleChildScrollView(
+        //     child: Column(
+        //   children: [
+        //     Container(
+        //       width: size.width,
+        //       height: 0.50,
+        //       decoration: BoxDecoration(
+        //         border: Border.all(
+        //           color: const Color(0xffc9d1d1),
+        //           width: 0.50,
+        //         ),
+        //       ),
+        //     ),
+        //     Padding(
+        //         padding: EdgeInsets.only(
+        //             left: size.width * 0.04,
+        //             right: size.width * 0.04,
+        //             top: size.height * 0.0357142857142857),
+        //         child: Center(
+        //           child: Text(politicas),
+        //         )),
+        //   ],
+        // )),
+        );
   }
 }
--- lib/src/screens/legal/therms_conditions_screen.dart
+++ lib/src/screens/legal/therms_conditions_screen.dart
@@ -1,65 +1,83 @@
-import 'package:ebloqs_app/src/global/therms.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class ThermsConditionsScreen extends StatelessWidget {
+class ThermsConditionsScreen extends StatefulWidget {
   static const routeName = 'ThermsConditionsScreen';
   const ThermsConditionsScreen({super.key});
+
   @override
+  State<ThermsConditionsScreen> createState() => _ThermsConditionsScreenState();
+}
+
+class _ThermsConditionsScreenState extends State<ThermsConditionsScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: const Text(
-          'Términos y condiciones',
-          style: TextStyle(
-            color: Color(0xff170658),
-            fontSize: 17,
-            fontFamily: 'Archivo',
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-        leadingWidth: size.width * 0.128,
-        leading: Padding(
-          padding: EdgeInsets.only(left: size.width * 0.064),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: SvgPicture.asset(
-              'assets/Vectores/Iconos/Arrow left.svg',
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: const Text(
+            'Términos y condiciones',
+            style: TextStyle(
+              color: Color(0xff170658),
+              fontSize: 17,
+              fontFamily: 'Archivo',
+              fontWeight: FontWeight.w700,
             ),
           ),
-        ),
-      ),
-      body: SingleChildScrollView(
-          child: Column(
-        children: [
-          Container(
-            width: size.width,
-            height: 0.50,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: const Color(0xffc9d1d1),
-                width: 0.50,
+          leadingWidth: size.width * 0.128,
+          leading: Padding(
+            padding: EdgeInsets.only(left: size.width * 0.064),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: SvgPicture.asset(
+                'assets/Vectores/Iconos/Arrow left.svg',
               ),
             ),
           ),
-          Padding(
-              padding: EdgeInsets.only(
-                  left: size.width * 0.04,
-                  right: size.width * 0.04,
-                  top: size.height * 0.0357142857142857),
-              child: Center(
-                child: Text(terminos),
-              )),
-        ],
-      )),
-    );
+        ),
+        body: const WebView(
+          initialUrl: 'https://ebloqs.com/ebloqs-token/terminos-y-condiciones',
+        )
+
+        //  SingleChildScrollView(
+        //     child: Column(
+        //   children: [
+        //     Container(
+        //       width: size.width,
+        //       height: 0.50,
+        //       decoration: BoxDecoration(
+        //         border: Border.all(
+        //           color: const Color(0xffc9d1d1),
+        //           width: 0.50,
+        //         ),
+        //       ),
+        //     ),
+        //     Padding(
+        //         padding: EdgeInsets.only(
+        //             left: size.width * 0.04,
+        //             right: size.width * 0.04,
+        //             top: size.height * 0.0357142857142857),
+        //         child: Center(
+        //           child: Text(terminos),
+        //         )),
+        //   ],
+        // )),
+        );
   }
 }
